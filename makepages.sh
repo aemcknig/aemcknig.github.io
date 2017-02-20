8eff6a5a2460b7af0d72962e154bd9dc94495e0f
Allison McKnight <aemcknig@gmail.com>
2017-02-20 18:59:06 -0500

    Add script to generate pages with hero images

---
+++ makepages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir $1
+# $2 is base of image name - ex. periwinklenuthatch_254377282
+for SIZE in 4040x670 1440x450 1280x400 900x224 480x150
+do
+    echo "<html><body><img src=\"https://img1.etsystatic.com/site-assets/vesta-homepage-headers-v3/$2_$SIZE.jpg\"></body></html>" > $1/$1_$SIZE.html
+done
